369397771c73c8a48872025f4fbc537904f170c8
iamnana-tyc <nanatyc7@gmail.com>
2026-03-16 13:19:45 +0300

    Add circuitbreaker configuration

--- gateway/src/main/java/com/iamnana/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/com/iamnana/gateway/config/GatewayConfig.java
@@ -5,16 +5,17 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
+@Configuration
 public class GatewayConfig {
 
-   // @Bean
+    @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder){
         return builder.routes()
                 .route("product-service", r -> r
                         .path("/products/**")
                         .filters(f -> f
-                                .rewritePath("/products(?<segment>/?.*)", "/api/products${segment}"))
+                                .prefixPath("/api")
+                                .circuitBreaker(config -> config.setName("ecomBreaker")))
                         .uri("lb://PRODUCT-SERVICE"))
                 .route("user-service", r-> r
                         .path("/users/**")
