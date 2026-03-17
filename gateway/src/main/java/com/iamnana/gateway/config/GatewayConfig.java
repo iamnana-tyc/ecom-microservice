8b2506e2154361200ed686831a3541ad0bcc55f6
iamnana-tyc <nanatyc7@gmail.com>
2026-03-17 18:54:19 +0300

    Add key resolver

--- gateway/src/main/java/com/iamnana/gateway/config/GatewayConfig.java
+++ gateway/src/main/java/com/iamnana/gateway/config/GatewayConfig.java
@@ -1,50 +1,19 @@
 package com.iamnana.gateway.config;
 
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class GatewayConfig {
 
     @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder){
-        return builder.routes()
-                .route("product-service", r -> r
-                        .path("/products/**")
-                        .filters(f -> f
-                                .prefixPath("/api")
-                                .circuitBreaker(config -> config
-                                        .setName("ecomBreaker")
-                                        .setFallbackUri("forward:/fallback/products")
-                                ))
-                        .uri("lb://PRODUCT-SERVICE"))
-                .route("user-service", r-> r
-                        .path("/users/**")
-                        .filters(f -> f
-                                .prefixPath("/api")
-                                .circuitBreaker(config -> config
-                                        .setName("ecomBreaker")
-                                        .setFallbackUri("forward:/fallback/users")
-                                ))
-                        .uri("lb://USER-SERVICE"))
-                .route("order-service", r -> r
-                        .path("/orders/**", "/cart/**")
-                        .filters(f -> f
-                                .prefixPath("/api")
-                                .circuitBreaker(config -> config
-                                        .setName("ecomBreaker")
-                                        .setFallbackUri("forward:/fallback/orders")
-                                ))
-                        .uri("lb://ORDER-SERVICE"))
-                .route("eureka-server" , r -> r
-                        .path("/eureka/main")
-                        .filters(f -> f.rewritePath("/eureka/main", "/"))
-                        .uri("http://localhost:8761"))
-                .route("eureka-server-static", r -> r
-                        .path("/eureka/**")
-                        .uri("http://localhost:8761"))
-                .build();
+    public KeyResolver hostNameKeyResolver() {
+        return exchange -> Mono.just(
+                exchange.getRequest()
+                        .getRemoteAddress()
+                        .getHostName()
+        );
     }
 }
