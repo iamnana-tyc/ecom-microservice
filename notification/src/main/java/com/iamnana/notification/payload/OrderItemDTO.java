44505282cceee025bcdc2ae5718423ec60c5a1f1
iamnana-tyc <nanatyc7@gmail.com>
2026-03-19 16:39:39 +0300

    Add OrderEventConsumer dto

---
+++ notification/src/main/java/com/iamnana/notification/payload/OrderItemDTO.java
@@ -0,0 +1,16 @@
+package com.ecommerce.order.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+public class OrderItemDTO {
+    private Long id;
+    private String productId;
+    private BigDecimal price;
+    private Integer quantity;
+    private BigDecimal subTotal; // this is the price * quantity
+}
