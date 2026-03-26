6fc003a12ca6bcd67531be32edda824ffeb401d2
iamnana-tyc <nanatyc7@gmail.com>
2026-03-26 13:45:14 +0300

    Add consumer method

--- notification/src/main/java/com/iamnana/notification/OrderEventConsumer.java
+++ notification/src/main/java/com/iamnana/notification/OrderEventConsumer.java
@@ -1,14 +1,21 @@
 package com.iamnana.notification;
 
 import com.iamnana.notification.payload.OrderCreatedEvent;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Consumer;
 
 
 @Service
+@Slf4j
 public class OrderEventConsumer {
-    @RabbitListener(queues = "${rabbitmq.queue.name}")
-    public void handleOrderEvent(OrderCreatedEvent orderCreatedEvent){
-        System.out.println("Received Order Event: " + orderCreatedEvent);
+    @Bean
+    public Consumer<OrderCreatedEvent> orderCreated(){
+        return event -> {
+            log.info("Received order created event for order: {}", event.getOrderId());
+            log.info("Received order created event for user id: {}", event.getUserId());
+        };
     }
 }
