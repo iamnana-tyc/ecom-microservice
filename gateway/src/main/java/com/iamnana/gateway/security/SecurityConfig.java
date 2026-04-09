9b923e9f18350592e21913bbf4e9f0689c8de08a
iamnana-tyc <nanatyc7@gmail.com>
2026-04-10 01:21:21 +0300

    Implement kecloak service

--- gateway/src/main/java/com/iamnana/gateway/security/SecurityConfig.java
+++ gateway/src/main/java/com/iamnana/gateway/security/SecurityConfig.java
@@ -2,10 +2,19 @@ package com.iamnana.gateway.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -15,10 +24,38 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .authorizeExchange(exchange -> exchange.anyExchange().authenticated())
-                .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
+                .authorizeExchange(exchange -> exchange
+//                        .pathMatchers("/users/**").hasRole("USER")
+//                        .pathMatchers("/products/**").hasRole("PRODUCT")
+//                        .pathMatchers("/orders/**").hasRole("ORDER")
+                        .anyExchange().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(
+                        jwt -> jwt.jwtAuthenticationConverter(grantedAuthoritiesExtractor())))
                 .build();
 
+    }
+
+    /**
+     * This is a method that converts user roles from jwt. so it extract user roles from token.
+     * @return
+     */
+    public Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor(){
+        ReactiveJwtAuthenticationConverter jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            List<String> roles = jwt.getClaimAsMap("resource_access")
+                    .entrySet()
+                    .stream()
+                    .filter(entry -> entry.getKey().equals("oauth-ecom-pkce"))
+                    .flatMap(entry -> ((Map<String, List<String>>) entry.getValue())
+                            .get("roles").stream())
+                    .toList();
+
+            System.out.println("Extracted Roles: " + roles);
+
+            return Flux.fromIterable(roles)
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role));
+        });
+        return jwtAuthenticationConverter;
     }
 
 }
