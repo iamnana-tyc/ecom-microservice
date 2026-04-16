4b59e6be2e1e56b81414b00325718f346f5a8786
iamnana-tyc <nanatyc7@gmail.com>
2026-04-17 00:25:20 +0300

    Add build image file to run images

---
+++ Deploy/Docker/build-images-buildpacks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ../..
+
+### BUILD ALL SERVICES
+cd configserver && ./mvnw spring-boot:build-image -DskipTests && cd ..
+cd eureka && ./mvnw spring-boot:build-image -DskipTests && cd ..
+cd order && ./mvnw spring-boot:build-image -DskipTests && cd ..
+cd user && ./mvnw spring-boot:build-image -DskipTests && cd ..
+cd product && ./mvnw spring-boot:build-image -DskipTests && cd ..
+cd gateway && ./mvnw spring-boot:build-image -DskipTests && cd ..
