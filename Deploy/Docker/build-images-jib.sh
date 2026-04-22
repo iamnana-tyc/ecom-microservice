035696c6a549d46c9c8bc274021a58b7db3b43d3
iamnana-tyc <nanatyc7@gmail.com>
2026-04-22 11:50:24 +0300

    Add build file for jib approach

---
+++ Deploy/Docker/build-images-jib.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ../..
+
+### BUILD ALL SERVICES
+cd configserver && ./mvnw clean compile jib:build && cd ..
+cd eureka && ./mvnw clean compile jib:build && cd ..
+cd order && ./mvnw clean compile jib:build && cd ..
+cd user && ./mvnw clean compile jib:build && cd ..
+cd product && ./mvnw clean compile jib:build && cd ..
+cd gateway && ./mvnw clean compile jib:build && cd ..
+cd notification && ./mvnw clean compile jib:build && cd ..
