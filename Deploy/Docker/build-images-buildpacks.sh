69ba08896126df3a4a5735be67d7880f59633eb8
iamnana-tyc <nanatyc7@gmail.com>
2026-04-22 11:38:48 +0300

    Buildpacks for building all images

--- Deploy/Docker/build-images-buildpacks.sh
+++ Deploy/Docker/build-images-buildpacks.sh
@@ -2,11 +2,41 @@
 
 cd ../..
 
-### BUILD ALL SERVICES
-cd configserver && ./mvnw spring-boot:build-image -DskipTests && cd ..
-cd eureka && ./mvnw spring-boot:build-image -DskipTests && cd ..
-cd order && ./mvnw spring-boot:build-image -DskipTests && cd ..
-cd user && ./mvnw spring-boot:build-image -DskipTests && cd ..
-cd product && ./mvnw spring-boot:build-image -DskipTests && cd ..
-cd gateway && ./mvnw spring-boot:build-image -DskipTests && cd ..
-cd notification && ./mvnw spring-boot:build-image -DskipTests && cd ..
+set -e
+
+services=(
+  configserver
+  eureka
+  order
+  user
+  product
+  gateway
+  notification
+)
+
+for s in "${services[@]}"; do
+  echo "Building $s"
+
+  cd "$s"
+
+  ./mvnw spring-boot:build-image -DskipTests || {
+    echo "Retrying $s..."
+    ./mvnw spring-boot:build-image -DskipTests
+  }
+
+  cd ..
+done
+
+
+##!/bin/bash
+#
+#cd ../..
+#
+#### BUILD ALL SERVICES
+#cd configserver && ./mvnw spring-boot:build-image -DskipTests && cd ..
+#cd eureka && ./mvnw spring-boot:build-image -DskipTests && cd ..
+#cd order && ./mvnw spring-boot:build-image -DskipTests && cd ..
+#cd user && ./mvnw spring-boot:build-image -DskipTests && cd ..
+#cd product && ./mvnw spring-boot:build-image -DskipTests && cd ..
+#cd gateway && ./mvnw spring-boot:build-image -DskipTests && cd ..
+#cd notification && ./mvnw spring-boot:build-image -DskipTests && cd ..
